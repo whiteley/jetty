5af34bc183c08f77157dae141c369e2f9ca895c3
Ian Coffey <ian.coffey@gmail.com>
2013-06-03 16:14:47 -0400

    symbol access to string access, clean up attrs

--- providers/app.rb
+++ providers/app.rb
@@ -20,14 +20,11 @@ action :create do
   end
 
   r << nginx_rproxy("server_#{new_resource.app}") do
-    app_name new_resource.app
-    data_path new_resource.data_path
     upstream_ports [new_resource.port]
     listen 81 # where does this come from in production, dna?
   end
 
   r << nginx_rproxy("server_#{new_resource.app}_https") do
-    data_path new_resource.data_path
     upstream_ports [new_resource.port]
     listen 444
   end
