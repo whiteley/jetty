42c6a18e5281a08da3f3b3a29c44e3249cc95e57
Jim Lindley & Ryan Souza <jlindley@engineyard.com>
2013-07-16 17:50:18 -0700

    Install jetty via deb url

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+# URL to download jetty deb from
+default["jetty"]["deb_url"] = "http://repo2.maven.org/maven2/org/mortbay/jetty/dist/jetty-deb/8.1.11.v20130520/jetty-deb-8.1.11.v20130520.deb"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,17 @@
+include_recipe 'java'
 include_recipe 'nginx'
 
-package "jetty-hightide-server"
+jetty_tmp_path = "/tmp/" + File.basename(node["jetty"]["deb_url"])
+
+remote_file "jetty" do
+  path jetty_tmp_path
+  source node["jetty"]["deb_url"]
+
+  not_if "dpkg-query -S jetty"
+  notifies :install, "package[jetty]", :immediately
+end
+
+package "jetty" do
+  action :nothing
+  source jetty_tmp_path
+end
